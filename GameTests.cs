b6102c9c3ba0d451039463db7a6cf3cd3af55e63
Clevergua <53552547+Clevergua@users.noreply.github.com>
2022-06-06 08:05:52 +0800

    更新

--- GameTests.cs
+++ GameTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Game;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,6 +12,12 @@ namespace GameTests
         {
             var dungeon = new Dungeon();
             dungeon.Update(0.1f);
+        }
+        [TestMethod]
+        public void 临时测试()
+        {
+            var b = new Stone() as Block;
+            System.Console.WriteLine(b.GetType());
         }
     }
 }
